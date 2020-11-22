7a8decafed9c6846ee87cb8daedc03dcd46bbe95
阿部直也 <n.abe@gemcook.com>
2020-11-22 14:38:25 +0900

    開発環境の初期設定

---
+++ src/components/Sample.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <button className='button' onClick={() => console.log('button')}>
+        ボタン
+      </button>
+    </div>
+  );
+};
+
+export default Sample;
