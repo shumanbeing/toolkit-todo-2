4620785347c3834486a6000ef37186513175d0c7
ShuNakajima <express.your.mind.0909@gmail.com>
2021-02-01 16:20:56 +0900

    firebaseの導入

---
+++ src/app/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+export const db = firebase.firestore();
+export const auth = firebase.auth();
